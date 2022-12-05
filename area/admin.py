66af6c53dd8db6a1f4ef21e23058b1dd2df788e9
wangdong2516 <1277431229@qq.com>
2022-12-05 16:07:35 +0800

    注册area到管理后台

--- area/admin.py
+++ area/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from area.models import ProvinceModel
+from area.models import CityModel
+from area.models import CountyModel
+from area.models import TownModel
 # Register your models here.
+
+
+admin.site.register(ProvinceModel)
+admin.site.register(CityModel)
+admin.site.register(CountyModel)
+admin.site.register(TownModel)
