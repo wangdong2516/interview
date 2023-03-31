3237025cb4c81133a416e9c94c01b5d1978d9fe2
wangdong2516 <1277431229@qq.com>
2023-03-31 21:39:44 +0800

    集成kafka，将日志消息发送到kafka

--- settings/base_settings.py
+++ settings/base_settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 
-import environ
 from pathlib import Path
+import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,86 +27,86 @@ env.read_env(f'{BASE_DIR}/envs/.env.{env.str("PROJECT_ENV", "dev")}')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'user',
-    'rest_framework',
+    "daphne",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "user",
+    "rest_framework",
     # django-cid应用(日志请求，记录唯一id)
-    'cid.apps.CidAppConfig',
-    'area',
-    'utils',
+    "cid.apps.CidAppConfig",
+    "area",
+    "utils",
     # 查询过滤
     # 数据分析模块
-    'analysis',
-    'django_filters',
+    "analysis",
+    "django_filters",
     # django-channels的网络聊天室
-    'chat',
+    "chat",
     # django-elasticsearch-dsl
-    'django_elasticsearch_dsl',
+    "django_elasticsearch_dsl",
 ]
 
 MIDDLEWARE = [
     # 日志唯一ID生成(django-cid)，中间件负责从 HTTP 请求标头获取相关属性
-    'cid.middleware.CidMiddleware',
+    "cid.middleware.CidMiddleware",
     # 日志处理中间件(记录请求日志信息)
-    'middleware.log_middleware.LogMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "middleware.log_middleware.LogMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'interview.urls'
+ROOT_URLCONF = "interview.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'interview.wsgi.application'
-ASGI_APPLICATION = 'interview.asgi.application'
+WSGI_APPLICATION = "interview.wsgi.application"
+ASGI_APPLICATION = "interview.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'interview',
-        'URL': env.str('DATABASES_URL'),
-        'USER': env.str('DATABASE_USER'),
-        'PORT': env.int('DATABASE_PORT'),
-        'PASSWORD': env.str('DATABASE_PASSWORD'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "interview",
+        "URL": env.str("DATABASES_URL"),
+        "USER": env.str("DATABASE_USER"),
+        "PORT": env.int("DATABASE_PORT"),
+        "PASSWORD": env.str("DATABASE_PASSWORD"),
     }
 }
 
@@ -114,16 +116,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -131,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -143,28 +145,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 # 静态文件的存放目录
-STATIC_ROOT = 'static'
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # 用户模型类
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = "user.User"
 
 # ------------celery配置项------------------
 # 设置celery的时区
 CELERY_TIMEZONE = "Asia/Shanghai"
 # 设置celery broker(存储任务的)的地址
-CELERY_BROKER_URL = env.str('BROKER_URL')
+CELERY_BROKER_URL = env.str("BROKER_URL")
 # BACKEND配置(存储结果),使用redis
-CELERY_RESULT_BACKEND = env.str('RESULT_BACKEND')
+CELERY_RESULT_BACKEND = env.str("RESULT_BACKEND")
 # 结果序列化方案
-CELERY_RESULT_SERIALIZER = 'json'
-# CELERYBEAT_SCHEDULER = 'celery.schedulers.DatabaseScheduler'  # 使用了django-celery默认的数据库调度模型,任务执行周期都被存在你指定的orm数据库中
+CELERY_RESULT_SERIALIZER = "json"
+# 使用了django-celery默认的数据库调度模型,任务执行周期都被存在你指定的orm数据库中
+# CELERYBEAT_SCHEDULER = 'celery.schedulers.DatabaseScheduler'
 # 存储任务结果的过期时间，超过这个时间自动过期，设置为None，表示永久有效
 CELERY_TASK_RESULT_EXPIRES = 1200
 # 每个worker可以同时处理的任务数量，默认是2，建议设置为当前电脑的核心数
@@ -176,98 +179,85 @@ CELERYD_MAX_TASKS_PER_CHILD = 200
 # 如果消息没有指定路由或者没有指定队列，都将放到默认队列中
 CELERY_DEFAULT_QUEUE = "default_wj"
 # 允许接受的内容类型，如果不符合该类型，则任务会被丢弃，支持的类型有json，YAML， pickle等
-CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_ACCEPT_CONTENT = ["application/json"]
 # 任务信息的序列化方式
-CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = "json"
 
 # -------------------百度OCR相关配置-------------------------
-APP_ID = '28335545'
-API_KEY = 'cZGotxafOdK8ThvYIuuOaGHs'
-OCR_SECRET_KEY = 'PvckkIsoxSeVUz1qSGmplKBELqthOBpv'
+APP_ID = "28335545"
+API_KEY = "cZGotxafOdK8ThvYIuuOaGHs"
+OCR_SECRET_KEY = "PvckkIsoxSeVUz1qSGmplKBELqthOBpv"
 
 
 # ----------------Django Rest Frame work相关的配置--------------
 REST_FRAMEWORK = {
     # 默认的分页器
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     # 每页的个数
-    'PAGE_SIZE': 10,
+    "PAGE_SIZE": 10,
     # 默认的过滤器后端
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    )
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
 # ---------------Django-Cid配置----------------------------------
 # https://django-correlation-id.readthedocs.io/en/latest/installation.html
 CID_GENERATE = True  # 默认情况下使用uuid4生成
-CID_RESPONSE_HEADER = 'Request-ID'
+CID_RESPONSE_HEADER = "Request-ID"
 CID_HEADER = CID_RESPONSE_HEADER
 
 
 # --------------日志相关配置-------------------------------
 # 日志文件的存放路径(不存在就创建)
-LOG_DIR = BASE_DIR / 'logs'
+LOG_DIR = BASE_DIR / "logs"
 if not LOG_DIR.exists():
     LOG_DIR.mkdir()
 
 LOGGING = {
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '[cid: %(cid)s] %(levelname)s %(asctime)s %(message)s'
-        },
-        'simple': {
-            'format': '[cid: %(cid)s] %(levelname)s %(message)s'
-        },
+    "version": 1,
+    "formatters": {
+        "verbose": {"format": "[cid: %(cid)s] %(levelname)s %(asctime)s %(message)s"},
+        "simple": {"format": "[cid: %(cid)s] %(levelname)s %(message)s"},
     },
-    'handlers': {
-        'api_request_handler': {
-            'level': 'INFO',
-            'class': 'utils.log_file_handler.CommonTimedRotatingFileHandler',
-            'formatter': 'verbose',
-            'filters': ['correlation'],
-            'filename': f'{BASE_DIR}/logs/api_request.log',
+    "handlers": {
+        "api_request_handler": {
+            "level": "INFO",
+            "class": "utils.log_file_handler.KafkaLoggingHandler",
+            "formatter": "verbose",
+            "filters": ["correlation"],
+            # 'filename': f'{BASE_DIR}/logs/api_request.log',
             # 每分钟切割一次日志
-            'when': 'midnight',
+            # 'when': 'midnight',
             # 时间间隔
-            'interval': 1,
+            # 'interval': 1,
             # 保留5份日志
-            'backupCount': 30,
-            'encoding': 'utf-8'
+            # 'backupCount': 30,
+            # 'encoding': 'utf-8'
         },
     },
-    'filters': {
-        'correlation': {
-            '()': 'cid.log.CidContextFilter'
-        },
+    "filters": {
+        "correlation": {"()": "cid.log.CidContextFilter"},
     },
-    'loggers': {
-        'api_request': {
-            'handlers': ['api_request_handler'],
-            'filters': ['correlation'],
-            'propagate': True,
-            'level': 'INFO'
+    "loggers": {
+        "api_request": {
+            "handlers": ["api_request_handler"],
+            "filters": ["correlation"],
+            "propagate": True,
+            "level": "INFO",
         },
     },
 }
 
 # ----------------sentry配置-------------------------
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
 sentry_sdk.init(
-dsn="http://895fa50f68214d3ab2700de2ef915c69@127.0.0.1:9000/3",
+    dsn="http://895fa50f68214d3ab2700de2ef915c69@127.0.0.1:9000/3",
     integrations=[DjangoIntegration()],
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
 
 
@@ -283,8 +273,6 @@ CHANNEL_LAYERS = {
 }
 
 # Elasticsearch配置
-ELASTICSEARCH_DSL={
-    'default': {
-        'hosts': 'localhost:9200'
-    },
+ELASTICSEARCH_DSL = {
+    "default": {"hosts": "localhost:9200"},
 }
--- utils/log_file_handler.py
+++ utils/log_file_handler.py
@@ -1,18 +1,26 @@
 """
     由于logging模块是线程安全的，但是在多线程环境下，无法保证多线程安全，这里重写TimedRotatingFileHandler
 """
+import json
+import logging
 import os
 import time
 from logging.handlers import TimedRotatingFileHandler
+from kafka import KafkaProducer
 
 
 class CommonTimedRotatingFileHandler(TimedRotatingFileHandler):
     """
-        重写TimedRotatingFileHandler逻辑
+    重写TimedRotatingFileHandler逻辑
     """
 
     @property
     def dfn(self):
+        """
+
+        Returns:
+
+        """
         current_time = int(time.time())
         # get the time that this sequence started at and make it a TimeTuple
         dst_now = time.localtime(current_time)[-1]
@@ -28,7 +36,9 @@ class CommonTimedRotatingFileHandler(TimedRotatingFileHandler):
                 else:
                     addend = -3600
                 time_tuple = time.localtime(t + addend)
-        dfn = self.rotation_filename(self.baseFilename + "." + time.strftime(self.suffix, time_tuple))
+        dfn = self.rotation_filename(
+            self.baseFilename + "." + time.strftime(self.suffix, time_tuple)
+        )
         return dfn
 
     def shouldRollover(self, record):
@@ -82,13 +92,69 @@ class CommonTimedRotatingFileHandler(TimedRotatingFileHandler):
             newRolloverAt = newRolloverAt + self.interval
 
         # If DST changes and midnight or weekly rollover, adjust for this.
-        if (self.when == 'MIDNIGHT' or self.when.startswith('W')) and not self.utc:
+        if (self.when == "MIDNIGHT" or self.when.startswith("W")) and not self.utc:
             dstAtRollover = time.localtime(newRolloverAt)[-1]
             dstNow = time.localtime(currentTime)[-1]
             if dstNow != dstAtRollover:
-                if not dstNow:  # DST kicks in before next rollover, so we need to deduct an hour
+                if (
+                    not dstNow
+                ):  # DST kicks in before next rollover, so we need to deduct an hour
                     addend = -3600
                 else:  # DST bows out before next rollover, so we need to add an hour
                     addend = 3600
                 newRolloverAt += addend
         self.rolloverAt = newRolloverAt
+
+
+class KafkaLoggingHandler(logging.Handler):
+    """
+    Kafka日志处理器
+    """
+
+    def __init__(
+        self, bootstrap_servers=["localhost:9092"], topic="my_topic", tls=None
+    ):
+        super(KafkaLoggingHandler, self).__init__()
+        # 创建一个kafka生产者，向指定的主题发送消息
+        self.kafka_producer = KafkaProducer(
+            # kafka集群或者单机的地址
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda x: json.dumps(x).encode(),
+            # 指定生产者在发送批次之前等待更多消息加入批次的时间，kafka生产者会在批次填满或者linger.ms达到
+            # 上限之后把批次发送出去
+            linger_ms=10,
+            tls=tls,
+        )
+
+        self.topic = topic
+
+    def emit(self, record: logging.LogRecord) -> None:
+        # 删除kafka的日志避免无限递归
+        if "kafka." in record.name:
+            return
+        try:
+            msg = self.format(record)
+            self.kafka_producer.send(self.topic, {"message": msg})
+            self.flush(timeout=1.0)
+        except Exception:
+            logging.Handler.handleError(self, record)
+
+    def flush(self, timeout=None) -> None:
+        """
+            刷新对象
+        Args:
+            timeout: 刷新的时间间隔
+
+        Returns:
+
+        """
+        self.kafka_producer.flush(timeout)
+
+    def close(self) -> None:
+        self.acquire()
+        try:
+            if self.kafka_producer:
+                self.kafka_producer.close()
+            logging.Handler.close(self)
+        finally:
+            self.release()
